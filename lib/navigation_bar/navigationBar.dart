23ffdb3fdaff44d7750d6e6ab8e33009cbad8761
Ashik-2001 <ashikbabu2001@gmail.com>
2022-06-16 22:00:44 +0530

    WIP: Created working navigationbar widget

---
+++ lib/navigation_bar/navigationBar.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+class NavBarr extends StatefulWidget {
+  const NavBarr({Key? key}) : super(key: key);
+
+  @override
+  State<NavBarr> createState() => _NavBarrState();
+}
+
+class _NavBarrState extends State<NavBarr> {
+  int _selectedIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: '',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: '',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: '',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: '',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.white,
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+
+  void _onItemTapped(int index){
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+}
