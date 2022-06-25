6c562fe0b29e0a0557e71f02f04f11a9c2d67751
Ashik-2001 <ashikbabu2001@gmail.com>
2022-06-25 16:41:28 +0530

    Completed POST method for creating complaints

--- lib/screens/complaint_page.dart
+++ lib/screens/complaint_page.dart
@@ -1,5 +1,7 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:hive/hive.dart';
+import 'package:http/http.dart' as http;
 
 class ScreenComplaint extends StatelessWidget {
   const ScreenComplaint({Key? key}) : super(key: key);
@@ -34,7 +36,7 @@ class ScreenComplaint extends StatelessWidget {
                           Navigator.push(
                             context,
                             MaterialPageRoute(
-                                builder: (context) => const CreateComplaint()),
+                                builder: (context) => CreateComplaint()),
                           );
                         },
                         icon: const ImageIcon(
@@ -56,7 +58,7 @@ class ScreenComplaint extends StatelessWidget {
         ));
   }
 
-  _widgetNoComplaints() {
+  Widget _widgetNoComplaints() {
     if (complaints_list == '') {
       return (const Padding(
         padding: EdgeInsets.fromLTRB(40.0, 10.0, 40.0, 0),
@@ -82,65 +84,124 @@ class CreateComplaint extends StatefulWidget {
 }
 
 class _CreateComplaintState extends State<CreateComplaint> {
+  final TextEditingController _controller = TextEditingController();
+  final _complaintskey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Container(
-        constraints: const BoxConstraints.expand(),
-        decoration: const BoxDecoration(
-            image: DecorationImage(
-          image: AssetImage('assets/backgrounds/bg.png'),
-          fit: BoxFit.cover,
-        )),
-        child: Scaffold(
-          backgroundColor: Colors.transparent,
-          body: Padding(
-            padding: const EdgeInsets.fromLTRB(30.0, 90, 30, 0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const SizedBox(
-                  width: double.infinity,
-                  child: Text(
-                    'Create new complaints',
-                    textAlign: TextAlign.start,
-                    style: TextStyle(fontSize: 30, color: Colors.white),
-                  ),
+      constraints: const BoxConstraints.expand(),
+      decoration: const BoxDecoration(
+          image: DecorationImage(
+        image: AssetImage('assets/backgrounds/bg.png'),
+        fit: BoxFit.cover,
+      )),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: Padding(
+          padding: const EdgeInsets.fromLTRB(30.0, 90, 30, 0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(
+                width: double.infinity,
+                child: Text(
+                  'Create new complaints',
+                  textAlign: TextAlign.start,
+                  style: TextStyle(fontSize: 30, color: Colors.white),
                 ),
-                const SizedBox(
-                  height: 20,
-                ),
-                const SizedBox(
-                  height: 50,
-                  child: TextField(
-                    style: TextStyle(color: Colors.white),
-                    decoration: InputDecoration(
-                      focusedBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.white),
-                      ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              SizedBox(
+                height: 50,
+                child: Form(
+                  key: _complaintskey,
+                  child: TextFormField(
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Enter the complaint';
+                      }
+                      return null;
+                    },
+                    controller: _controller,
+                    style: const TextStyle(color: Colors.white),
+                    decoration: const InputDecoration(
+                      // focusedBorder: OutlineInputBorder(
+                      //   borderSide: BorderSide(color: Colors.white),
+                      // ),
                       hintText: 'Enter new complaints here',
-                      fillColor: Colors.white,
+                      // fillColor: Colors.white,
                       contentPadding: EdgeInsets.symmetric(
                           vertical: 20.0, horizontal: 10.0),
                       // filled: true
                     ),
-                    maxLines: 5,
-                    minLines: 3,
+                    // maxLines: 5,
+                    // minLines: 3,
                   ),
                 ),
-                const SizedBox(
-                  height: 20,
-                ),
-                ElevatedButton(
-                    onPressed: () {
-                      Navigator.pop(context);
-                    },
-                    style: ElevatedButton.styleFrom(
-                        fixedSize: const Size(100, 40),
-                        primary: Colors.white38),
-                    child: const Text('Submit'))
-              ],
-            ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              ElevatedButton(
+                onPressed: () async {
+                  print(_controller.text);
+                  if (_complaintskey.currentState!.validate()) {
+                    var status =
+                        await _postComplaints(2019068, 404, _controller.text);
+                    if (status.toString() == 'success') {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(
+                          content: Text(status),
+                        ),
+                      );
+                      _popComplaintReg();
+                      
+                    } else {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(
+                          content: Text(status),
+                        ),
+                      );
+                    }
+                    // If the form is valid, display a snackbar. In the real world,
+                    // you'd often call a server or save the information in a database.
+
+                  }
+                },
+                style: ElevatedButton.styleFrom(
+                    fixedSize: const Size(100, 40), primary: Colors.white38),
+                child: const Text('Submit'),
+              )
+            ],
           ),
-        ));
+        ),
+      ),
+    );
+  }
+
+  Future<String> _postComplaints(admNo, roomNo, compDesc) async {
+    Map<String, dynamic> complaintData = {'admission_no':admNo,'room_no':roomNo,'complaint_desc':compDesc};
+    try {
+      var response = await http.post(
+        Uri.parse('http://127.0.0.1:8000/api/complaints/'),
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
+        },
+        body: jsonEncode(
+          complaintData
+        ),
+      );
+      print(response.body);
+      var status = jsonDecode(response.body);
+      return (status["status"]);
+    } catch (e) {
+      return 'Unsuccessfull';
+    }
+  }
+  _popComplaintReg(){
+    return (Navigator.pop(context));
   }
 }
